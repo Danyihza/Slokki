2db02e538653fb8354f2b2cb0037b6f03c4f761e
arinifir <55473280+arinifir@users.noreply.github.com>
2021-12-28 15:36:32 +0700

    table owner, bahan baku

---
+++ database/migrations/2021_12_28_083038_create_owner_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOwnerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('owner', function (Blueprint $table) {
+            $table->string('id_owner', 100)->primary();
+            $table->string('nama_owner', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('owner');
+    }
+}
