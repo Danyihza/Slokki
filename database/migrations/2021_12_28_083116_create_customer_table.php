4cca7bca167aa3dbb59e7b623e36cdee63c7dc38
Danyihza <danyahmadihza99@gmail.com>
2021-12-28 15:32:28 +0700

    migration

---
+++ database/migrations/2021_12_28_083116_create_customer_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->string('id_customer', 100)->primary();
+            $table->string('nama_customer', 100);
+            $table->text('alamat_customer');
+            $table->string('no_telp', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
