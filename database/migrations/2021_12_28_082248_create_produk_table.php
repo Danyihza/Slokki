af4e411a2096c2df280cb41b3e66372dc16e8243
Danyihza <danyahmadihza99@gmail.com>
2021-12-28 15:27:02 +0700

    migration

--- database/migrations/2021_12_28_082248_create_produk_table.php
+++ database/migrations/2021_12_28_082248_create_produk_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProdukTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('produk', function (Blueprint $table) {
+            $table->string('id_produk', 100)->primary();
+            $table->string('nama_produk', 100);
+            $table->string('jenis_produk', 100);
+            $table->integer('harga_jual');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('produk');
     }
 }
